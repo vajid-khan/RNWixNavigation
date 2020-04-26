9e1e3faef0b4ed0b1be481d9507437e57d3b2086
vajid khan <khanvajid04@gmail.com>
2020-04-26 21:44:37 +0530

    firebase cloud messaging

--- index.js
+++ index.js
@@ -8,9 +8,15 @@
 
 // AppRegistry.registerComponent(appName, () => App);
 
+import messaging from '@react-native-firebase/messaging';
 
 import { Navigation } from "react-native-navigation";
 import regiserRoutes, { setAuthStack } from './src/router/registerRoutes';
 regiserRoutes();
+
+// Register background handler
+messaging().setBackgroundMessageHandler(async remoteMessage => {
+    console.log('Message handled in the background!', remoteMessage);
+});
 
 Navigation.events().registerAppLaunchedListener(setAuthStack);
--- src/screens/login.tsx
+++ src/screens/login.tsx
@@ -1,16 +1,40 @@
-import React from "react";
+import React, { useEffect } from "react";
 import {
     View,
     Text,
+    Alert,
     Button,
     StyleSheet,
 } from "react-native";
 
 import { setAppNavigationStack } from '../router/registerRoutes'
+import messaging from '@react-native-firebase/messaging';
 
 import { Navigation } from 'react-native-navigation'
 
 const login = (props) => {
+
+    useEffect(() => {
+        requestUserPermission();
+    }, []);
+
+    useEffect(() => {
+        const unsubscribe = messaging().onMessage(async remoteMessage => {
+            Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
+        });
+
+        return unsubscribe;
+    }, []);
+
+    async function requestUserPermission() {
+        const settings = await messaging().requestPermission();
+
+        if (settings) {
+            console.log('Permission settings:', settings);
+        }
+    }
+
+
     return (
         <View style={styles.container}>
             <Text>login</Text>
